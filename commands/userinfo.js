f0608917dc999e6d320dbd1b42b464a5e2a74ef2
InfernalFire <32463491+InfernalFire@users.noreply.github.com>
2017-10-02 12:57:48 -0500

    Update userinfo.js

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js')
-exports.run = (client, message, args')
+exports.run = (client, message, args)
   let member = message.mentions.members.first()
   let user = message.mentions.users.first()
   let embed= new Discord.RichEmbed()

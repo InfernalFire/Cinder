e65f1be79c2c0395b922d3a2eb8051f071a26bce
InfernalFire <32463491+InfernalFire@users.noreply.github.com>
2017-10-02 12:57:14 -0500

    Create userinfo.js

---
+++ commands/userinfo.js
@@ -0,0 +1,22 @@
+const Discord = require('discord.js')
+exports.run = (client, message, args')
+  let member = message.mentions.members.first()
+  let user = message.mentions.users.first()
+  let embed= new Discord.RichEmbed()
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+ message.channel.send(embed)

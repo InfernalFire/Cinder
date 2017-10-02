edd648146bc11d6a05c631c081bced3e17f13f3f
InfernalFire <32463491+InfernalFire@users.noreply.github.com>
2017-10-02 13:05:02 -0500

    Update setstatus.js

--- commands/setstatus.js
+++ commands/setstatus.js
@@ -3,7 +3,7 @@ const config = require('../config.json')
   exports.run = (client, message, args) => {
   let embed = new Discord.RichEmbed()
   
-  .setAuthor(message.guild, client.user.displayAvatarURL')
+  .setAuthor(message.guild, client.user.displayAvatarURL)
   .setDescription('**This command sets the status of the bot.**')
   .setFooter(`This message has been sent from the ${message.guild} server.`)
   .setColor('#f00000')

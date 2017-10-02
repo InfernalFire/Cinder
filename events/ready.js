160fb13ef97b278002c8687d424345e6c12c53a1
InfernalFire <32463491+InfernalFire@users.noreply.github.com>
2017-10-02 12:54:36 -0500

    Ready Event

---
+++ events/ready.js
@@ -0,0 +1,5 @@
+exports.run = (client) => {
+  console.log(`Ready in ${client.guilds.size}.`)
+  console.log(`Ready in ${client.channels.size}.`)
+  console.log(`Ready for ${client.users.size}.`)
+}

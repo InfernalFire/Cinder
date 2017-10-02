47bf611b1485d6eaf185e5c0b18a946603ea444b
InfernalFire <32463491+InfernalFire@users.noreply.github.com>
2017-10-02 13:04:49 -0500

    SetStatus | Dev Command

---
+++ commands/setstatus.js
@@ -0,0 +1,15 @@
+const Discord = require('discord.js')
+const config = require('../config.json')
+  exports.run = (client, message, args) => {
+  let embed = new Discord.RichEmbed()
+  
+  .setAuthor(message.guild, client.user.displayAvatarURL')
+  .setDescription('**This command sets the status of the bot.**')
+  .setFooter(`This message has been sent from the ${message.guild} server.`)
+  .setColor('#f00000')
+  .addField('')
+  if(message.author.id !== config.ownerID) return;
+  
+  message.channel.send(embed)
+
+}

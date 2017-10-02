51cdfd2bc2164c3bc387f44b5768dddbfaa5eb68
InfernalFire <32463491+InfernalFire@users.noreply.github.com>
2017-10-02 12:59:17 -0500

    ServerInfo Command

---
+++ commands/serverinfo.js
@@ -0,0 +1,8 @@
+const Discord = require('discord.js')
+exports.run = (client, message, args) => {
+let member = message.mentions.members.first()
+let user = message.mentions.users.first()
+let embed = new Discord.RichEmbed()
+
+
+}

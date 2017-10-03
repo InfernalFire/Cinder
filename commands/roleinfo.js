3c8207bd02643fcea3ef6766aeac3fe9aa08368e
InfernalFire <32463491+InfernalFire@users.noreply.github.com>
2017-10-03 13:05:13 -0500

    Update roleinfo.js

--- commands/roleinfo.js
+++ commands/roleinfo.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js')
 exports.run = (client, message, args) => {
   let member = message.member
+  let user = message.member.users.first()
   let role = message.mentions.roles.first()
   let embed = new Discord.RichEmbed()
 }

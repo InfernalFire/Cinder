4ecde26cc2926268200228c19bdd382058e1adbf
InfernalFire <32463491+InfernalFire@users.noreply.github.com>
2017-10-03 12:54:12 -0500

    Update takerole.js

--- commands/takerole.js
+++ commands/takerole.js
@@ -1,16 +1,22 @@
 const Discord = require('discord.js')
-exports.run = (client, messsage, args) => {
+exports.run = (client, message, args) => {
   let role = message.mentions.roles.first()
-  let member = message.mentions.members.first()
-  let user = message.mentions.members.first()
+  let member = message.mentions.members.first() || message.member
+  let user = message.mentions.users.first() message.author
+  let modlog = message.guild.channels.find('name', 'mod-log')
+  if(!modlog) return message.reply(`Please create a channel named 'mod-log'`)
+  if(!role) return message.reply('Please include a role')
   var embed = new Discord.RichEmbed()
   
+   member.addRole(role).catch(console.error)
+  
   .setAuthor(message.guild, client.user.displayAvatarURL)
-  .setDescription('Takes a role from a user')
-  .setFooter(`This message has been sent from ${message.guild}.`)
+  .setDescription('Removes a role to a user.')
+  .setFooter(`This message has been sent at ${message.guild}.`)
   .addField('Member:', member)
-  .addField('Role', role)
- 
-
-member.removeRole(role).catch(console.error)
+  .addField('Role:', role)
+  
+  message.channel.send(` ${user} has added the role to ${member}. `)
+  modlog.send(embed)
+  user.send(embed)
 }

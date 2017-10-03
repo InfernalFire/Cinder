af1499df7d6c809a77debd9be6cd41b34f759c65
InfernalFire <32463491+InfernalFire@users.noreply.github.com>
2017-10-03 12:44:34 -0500

    Update takerole.js

--- commands/takerole.js
+++ commands/takerole.js
@@ -2,7 +2,14 @@ const Discord = require('discord.js')
 exports.run = (client, messsage, args) => {
   let role = message.mentions.roles.first()
   let member = message.mentions.members.first()
-  let user = message.mentions.members.first() || message.user
+  let user = message.mentions.members.first()
+  var embed = new Discord.RichEmbed()
+  
+  .setAuthor(message.guild, client.user.displayAvatarURL)
+  .setDescription('Takes a role from a user')
+  .setFooter(`This message has been sent from ${message.guild}.`)
+  .addField('Member:', member)
+  .addField('Role', role)
  
 
 member.removeRole(role).catch(console.error)

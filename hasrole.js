4515780981ff5e6a6784fba63d6318bc9c5de5e6
InfernalFire <32463491+InfernalFire@users.noreply.github.com>
2017-10-04 12:15:15 -0500

    Delete hasrole.js

--- hasrole.js
+++
@@ -1,7 +0,0 @@
-const Discord = require('discord.js')
-exports.run = (client, message, args) => {
-  let role = message.mentions.roles.first()
-  let membersWithRole = message.guild.roles.get(role).members
-  
-  message.channel.send(`Got ${membersWithRole.size} members with that role.`)
-}

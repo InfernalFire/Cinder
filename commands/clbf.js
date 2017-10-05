47b767c652bd1abd3b6e3e9b9c2b3aea872d7d8b
InfernalFire <32463491+InfernalFire@users.noreply.github.com>
2017-10-05 12:25:15 -0500

    Update clbf.js

--- commands/clbf.js
+++ commands/clbf.js
@@ -3,6 +3,7 @@ const config = require('../config.json')
 exports.run = (client, message, args) => {
   if(message.author.id !== config.ownerID) return message.reply('Insufficient Permissions.');
   if(!args) return message.reply('Please enter some valid arguments.')
+  let randomtag = message.guild
   let embed = new Discord.RichEmbed()
   
   .setAuthor('Bug Fixes')
